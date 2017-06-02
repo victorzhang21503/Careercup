49f0a1189dd4178ca8a569bdd05b7f02187570a5
mzhan123 <mzhan123@cpcom102macm52.care.asu.edu>
2017-06-01 17:40:46 -0700

    commit ranking combination

--- src/Others/RankingCombination.java
+++ src/Others/RankingCombination.java
@@ -3,51 +3,52 @@ package Others;
 public class RankingCombination {
 	public static void main(String[] args) {
 		RankingCombination solution = new RankingCombination();
-		System.out.println(solution.getRankingNumber(3));
-	}
-	
-	public int getRankingNumber(int n) {
-		int res = 0;
-		for(int i = 1; i <= n; i++) {
-			int teamNum = getTeams(n, i, 2, 1, 1);
-			//System.out.println(teamNum);
-			res += stairProduct(i, i) * teamNum;
-		}
-		return res;
-	}
-	
-	public int getTeams(int n, int teams, int pre, int cnt, int mul) {
-		
-		if(n == teams) {
-			return mul;
-		}
-		if(teams == 0) {
-			return 0;
-		}
-		
-		int res = 0;
-		for(int i = pre;  i <= n - teams + 1; i++) {
-			if(i == pre) {
-				int tmp = mul / cnt * C(n, i);
-				res += getTeams(n - i, teams - 1,  i, cnt + 1, tmp);
-			}else {
-				res += getTeams(n - i, teams - 1, i, 1, mul);
-			} 
-		}
-		
-		return res;
-	}
-	
-	public int C(int n, int k) {
-		return stairProduct(n, k) / stairProduct(k, k);
-	}
-	
-	
-	public int stairProduct(int n, int len){
-		int mul = 1;
-		for(int i = n; i > n - len; i--) {
-			mul *= i;
-		}
-		return mul;
-	}
+	    System.out.println(solution.getRankingNumber(4));
+	  }
+	  
+	  public int getRankingNumber(int n) {
+	    int res = 0;
+	    for(int i = 1; i <= n; i++) {
+	      int teamNum = getTeams(n, i, 2, 1, 1);
+	      //System.out.println(teamNum);
+	      res += stairProduct(i, i) * teamNum;
+	    }
+	    return res;
+	  }
+	  
+	  public int getTeams(int n, int teams, int pre, int cnt, int mul) {
+	    
+	    if(n == teams) {
+	      return mul;
+	    }
+	    if(teams == 0) {
+	      return 0;
+	    }
+	    
+	    int res = 0;
+	    for(int i = pre;  i <= n - teams + 1; i++) {
+	      if(i == pre) {
+	        int tmp = mul / cnt * C(n, i);
+	        res += getTeams(n - i, teams - 1,  i, cnt + 1, tmp);
+	      }else {
+	        int tmp = mul * C(n, i);
+	        res += getTeams(n - i, teams - 1, i, 1, tmp);
+	      } 
+	    }
+	    
+	    return res;
+	  }
+	  
+	  public int C(int n, int k) {
+	    return stairProduct(n, k) / stairProduct(k, k);
+	  }
+	  
+	  
+	  public int stairProduct(int n, int len){
+	    int mul = 1;
+	    for(int i = n; i > n - len; i--) {
+	      mul *= i;
+	    }
+	    return mul;
+	  }
 }
